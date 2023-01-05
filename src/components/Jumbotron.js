3b938484a99536d27ec2de1ada4bb9c39bc15125
Jitesh <vickeysingh152@gmail.com>
2023-01-05 18:41:25 +0530

    Commit Test

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,17 +1,24 @@
 import {Col} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-function Jumbotron() {
+
+function Jumbotron( props ) {
     return (
         <div className='jumbotron p-3 p-md-5 text-white rounded bg-dark'>
             <Col md={6} className='px-0'>
-                <h1 className="display-4 font-italic">Title of a longer featured blog post</h1>
+                <h1 className="display-4 font-italic">{props.heading}</h1>
                 <p className="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
                 <p className='lead mb-0'>
-                    <a href="#" className="text-white font-weight-bold">Continue reading...</a>
+                    <Link className="text-white font-weight-bold" to={props.path}>Continue reading...</Link>
                 </p>
             </Col>
         </div>
     );
+}
+
+Jumbotron.defaultProps = {
+    heading: "Title of a longer featured blog post",
+    path: '/',
 }
 
 export default Jumbotron;
